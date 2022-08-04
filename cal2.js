5407d8ddd25fd8f268092bc88a34d289352bbd56
Aj_shadow <105531661+Avijit-roy@users.noreply.github.com>
2022-08-04 14:18:50 +0530

    Update cal2.js

--- cal2.js
+++ cal2.js
@@ -14,3 +14,25 @@ function ere() {
 function zer() {
   document.getElementById("result").value = "";
 }
+function perce() {
+  let x = document.getElementById("result").value;
+  let y = x.length;
+  var k;
+  const z = ["+", "*", "-", "/"];
+  let i = y - 1;
+  loop1: for (i = y - 1; i > -1; i--) {
+    loop2: for (var j = 0; j <= 3; j++) {
+      if (x[i] == z[j]) {
+        k = i;
+        break loop1;
+      } else {
+        continue;
+      }
+    }
+  }
+  let md = x.substr(k + 1, y);
+  md = md / 100;
+  x = x.substr(0, k + 1);
+  x = x.concat(md);
+  document.getElementById("result").value = x;
+}
